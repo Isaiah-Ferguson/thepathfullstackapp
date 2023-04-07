49ca2abbd34f71fc2f3ce4fd6f1aadc789671882
Isaiah-Ferguson <crestice@yahoo.com>
2023-04-07 11:15:34 -0700

    Data Services changes

--- src/DataServices/DataServices.tsx
+++ src/DataServices/DataServices.tsx
@@ -1,57 +1,105 @@
 let userData = {};
-interface User {
-    name: string;
-    email: string;
-    password: string;
+
+
+  async function createAccount(CreatedUser : object) {
+      //We want to target our User Controller
+      const res = await fetch('https://thepathapi.azurewebsites.net/User/AddUser',{
+          method:"POST",
+          headers:{
+              'Content-Type':"application/json"
+          },
+          body:JSON.stringify(CreatedUser)
+      });
+      if(!res.ok){
+          const message = `An Error has Occured  ${res.status}`;
+          throw new Error(message);
+      }
+      const data = await res.json();
+      console.log(data);
+      //We are not writeing a return because this is a POST.
   }
-
-async function createAccount(CreatedUser: string) {
-    //We want to target our User Controller
-    const res = await fetch('https://thepathapi.azurewebsites.net/User/AddUser',{
-        method:"POST",
-        headers:{
-            'Content-Type':"application/json"
-        },
-        body:JSON.stringify(CreatedUser)
-    });
-    if(!res.ok){
-        const message = `An Error has Occured  ${res.status}`;
-        throw new Error(message);
-    }
-    const data = await res.json();
-    console.log(data);
-    //We are not writeing a return because this is a POST.
-}
-
-async function login(loginUser: object) {
-    const res = await fetch('https://thepathapi.azurewebsites.net/User/Login',{
-        method:"POST",
-        headers:{
-            'Content-Type':"application/json"
-        },
-        body:JSON.stringify(loginUser)
-    });
-    if(!res.ok){
-        const message = `An Error has Occured  ${res.status}`;
-        throw new Error(message);
-    }
-    const data = await res.json();
-    console.log(data);
-    //We are not writeing a return because this is a POST.
-    return data;
-}
-
-async function GetLoggedInUserData(username: string) {
-    let res = await fetch(`https://thepathapi.azurewebsites.net/User/userbyusername/${username}`)
-    let data = await res.json();
-    userData = data;
-    console.log(userData);
-}
-
-async function GetPublishedBlogItem() {
-    let res = await fetch(`https://isaiahblogtest.azurewebsites.net/Blog/GetPublishedItems/`)
-    let data = await res.json();
-    return data;
-}
-
-export { createAccount, login, GetPublishedBlogItem, GetLoggedInUserData }
+  
+  async function login(loginUser : object) {
+      const res = await fetch('https://thepathapi.azurewebsites.net/User/Login',{
+          method:"POST",
+          headers:{
+              'Content-Type':"application/json"
+          },
+          body:JSON.stringify(loginUser)
+      });
+      if(!res.ok){
+          const message = `An Error has Occured  ${res.status}`;
+          throw new Error(message);
+      }
+      const data = await res.json();
+      console.log(data);
+      //We are not writeing a return because this is a POST.
+      return data;
+  }
+  
+  async function GetLoggedInUserData(username: string) {
+      let res = await fetch(`https://thepathapi.azurewebsites.net/User/userbyusername/${username}`)
+      let data = await res.json();
+      userData = data;
+      return userData;
+  }
+  
+  async function GetPublishedBlogItem() {
+      let res = await fetch(`https://thepathapi.azurewebsites.net/Blog/GetPublishedItems/`)
+      let data = await res.json();
+      return data;
+  }
+  
+  function checkToken() {
+      let result = false;
+      let lsData = localStorage.getItem('Token');
+      if(lsData != null){
+          result = true;
+      }
+      return result;
+  }
+  
+  function loggedInData() {
+      return userData;
+      //this will consist of user ID and their Name.
+  }
+  
+  async function addBlogItem(blogItem: object) {
+      const res = await fetch('https://thepathapi.azurewebsites.net/blog/AddBlogItem',{
+          method:"POST",
+          headers:{
+              'Content-Type':"application/json"
+          },
+          body:JSON.stringify(blogItem)
+      });
+      if(!res.ok){
+          const message = `An Error has Occured  ${res.status}`;
+          throw new Error(message);
+      }
+      const data = await res.json();
+      return data;
+  }
+  
+  async function getBlogItemsByUserId(userId: string) {
+      console.log(userId);
+      let res = await fetch(`https://thepathapi.azurewebsites.net/blog/GetItemsByUserId/${userId}`)
+      let data = await res.json();
+      return data;
+  }
+  
+  async function updateBlogItem(blogItem : object) {
+      const res = await fetch('https://thepathapi.azurewebsites.net/blog/UpdateBlogitem',{
+          method:"POST",
+          headers:{
+              'Content-Type':"application/json"
+          },
+          body:JSON.stringify(blogItem)
+      });
+      if(!res.ok){
+          const message = `An Error has Occured  ${res.status}`;
+          throw new Error(message);
+      }
+      const data = await res.json();
+      return data;
+  }
+  export { createAccount, login ,GetLoggedInUserData, GetPublishedBlogItem, checkToken, loggedInData, addBlogItem, getBlogItemsByUserId, updateBlogItem }
