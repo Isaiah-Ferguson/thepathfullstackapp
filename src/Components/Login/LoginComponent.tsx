ef482f7ecc62a7dee8cce0889bab78e60dd93572
Isaiah-Ferguson <crestice@yahoo.com>
2023-05-10 14:14:25 -0700

    Login Loading Screen

--- src/Components/Login/LoginComponent.tsx
+++ src/Components/Login/LoginComponent.tsx
@@ -23,12 +23,20 @@ export default function LoginComponent() {
         console.log(token)
         if(token.token != null){
           setUserToast(true);
-          localStorage.setItem("Token", token.token);
-          await GetLoggedInUserData(username);
-          const loggedIn = loggedInData();
-          sessionStorage.setItem('loggedIn', JSON.stringify(loggedIn));
-          data.setUserId(loggedIn.userId);
-          navigate("/profile");
+          setTimeout(() => {
+            const test =async ()=> {
+              localStorage.setItem("Token", token.token);
+              await GetLoggedInUserData(username);
+              const loggedIn = loggedInData();
+              sessionStorage.setItem('loggedIn', JSON.stringify(loggedIn));
+              data.setUserId(loggedIn.userId);
+              navigate("/profile");
+            }
+            test()
+   
+          }, 1000);
+          
+          
         }
     }
 
@@ -37,24 +45,26 @@ export default function LoginComponent() {
   return (
     <div className='loginBg'>
                    {userToast && (
-                <div className='Loading-Div'>
-                  <div className="load-wrapp">
-        <div className="load-6">
-          <div className="letter-holder">
-            <div className="l-1 letter">L</div>
-            <div className="l-2 letter">o</div>
-            <div className="l-3 letter">a</div>
-            <div className="l-4 letter">d</div>
-            <div className="l-5 letter">i</div>
-            <div className="l-6 letter">n</div>
-            <div className="l-7 letter">g</div>
-            <div className="l-8 letter">.</div>
-            <div className="l-9 letter">.</div>
-            <div className="l-10 letter">.</div>
-          </div>
+<div className='Loading-Div'>
+    <div className="load-wrapp">
+      <div className="load-6">
+        <div className="letter-holder">
+          <div className="l-1 letter">L</div>
+          <div className="l-2 letter">o</div>
+          <div className="l-3 letter">a</div>
+          <div className="l-4 letter">d</div>
+          <div className="l-5 letter">i</div>
+          <div className="l-6 letter">n</div>
+          <div className="l-7 letter">g</div>
+          <div className="l-8 letter">.</div>
+          <div className="l-9 letter">.</div>
+          <div className="l-10 letter">.</div>
         </div>
       </div>
-                </div>
+    </div>
+  
+  <div className="clear"></div>
+  </div>
               )}
     <Container className='d-flex justify-content-center mobileContainer' style={{paddingTop: 200}}>
         <Row className='wrapper' >
--- src/Components/MainFeedComponent/MainFeedEventComponent.tsx
+++ src/Components/MainFeedComponent/MainFeedEventComponent.tsx
@@ -89,22 +89,25 @@ export default function MainFeedEventComponent() {
               </h6>
             </Col>
           </Row>
-        ))) : (        <div className="load-wrapp">
-        <div className="load-6">
-          <div className="letter-holder">
-            <div className="l-1 letter">L</div>
-            <div className="l-2 letter">o</div>
-            <div className="l-3 letter">a</div>
-            <div className="l-4 letter">d</div>
-            <div className="l-5 letter">i</div>
-            <div className="l-6 letter">n</div>
-            <div className="l-7 letter">g</div>
-            <div className="l-8 letter">.</div>
-            <div className="l-9 letter">.</div>
-            <div className="l-10 letter">.</div>
-          </div>
+        ))) : (<>
+        <div className="load-wrapp">
+      <div className="load-6">
+        <div className="letter-holder">
+          <div className="l-1 letter">L</div>
+          <div className="l-2 letter">o</div>
+          <div className="l-3 letter">a</div>
+          <div className="l-4 letter">d</div>
+          <div className="l-5 letter">i</div>
+          <div className="l-6 letter">n</div>
+          <div className="l-7 letter">g</div>
+          <div className="l-8 letter">.</div>
+          <div className="l-9 letter">.</div>
+          <div className="l-10 letter">.</div>
         </div>
-      </div>)
+      </div>
+    </div>
+  
+  <div className="clear"></div></>)
       }
     </>
   );
